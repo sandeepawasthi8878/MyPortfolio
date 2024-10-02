891f58979a5753bb0d5a2d53ae8c17866ddff0d2
sandeep awasthi <sandeepawasthi8878@gmail.com>
2024-10-02 08:40:44 -0700

    firstcommit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+const typed = new Typed('.muliple-text', {
+    strings: ['Full Stack Developer', 'Web Designer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
+
